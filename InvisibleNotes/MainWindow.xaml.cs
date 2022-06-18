78713f5881bcfc2088f172cf187f746f844daaa7
Piotr Paszko <piotr.paszko.dev@gmail.com>
2022-06-18 18:08:37 +0200

    Implement basic version

--- InvisibleNotes/MainWindow.xaml.cs
+++ InvisibleNotes/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,63 @@ namespace InvisibleNotes
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string FileName = "note.txt";
+        private const int LinesCount = 25;
+
+        private int _currentLine = 0;
+        private List<string> _lines = new List<string>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            if (File.Exists(FileName))
+                NotesLabel.Content = "File note.txt not exists. Create file and fill with your notes.";
+
+            using var noteFile = File.OpenRead(FileName);
+            using var reader = new StreamReader(noteFile);
+            var content = reader.ReadToEnd();
+            _lines = content.Split('\n').ToList();
+            ShowPage();
+
+            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            this.Left = desktopWorkingArea.Right - this.Width;
+            this.Top = desktopWorkingArea.Bottom - this.Height;
+        }
+
+        private void AnyKeyDown(object sender, KeyEventArgs e)
+        {
+            if ((Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
+            {
+                if (Keyboard.IsKeyDown(Key.Q) && Keyboard.IsKeyDown(Key.Q))
+                {
+                    Environment.Exit(0);
+                }
+
+                if (Keyboard.IsKeyDown(Key.Down) && Keyboard.IsKeyDown(Key.Down))
+                {
+                    _currentLine++;
+                    ShowPage();
+                }
+
+                if (Keyboard.IsKeyDown(Key.Up) && Keyboard.IsKeyDown(Key.Up))
+                {
+                    _currentLine--;
+                    ShowPage();
+                }
+            }
+        }
+
+        private void MouseWheelMoved(object sender, MouseWheelEventArgs e)
+        {
+            MessageBox.Show("test");
+        }
+
+        private void ShowPage()
+        {
+            var content = _lines.Skip(_currentLine).Take(LinesCount).ToArray();
+            var page = string.Join('\n', content);
+            NotesLabel.Content = page;
         }
     }
 }
